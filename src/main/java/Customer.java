3c2130b9db258397fedbb73d51534e27d90e064d
yassine <yeladlouni@gmail.com>
2023-09-07 15:10:07 +0100

    Initial commit

---
+++ src/main/java/Customer.java
@@ -0,0 +1,65 @@
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private char gender;
+    private double salary;
+
+    public Customer(int id, String firstName, String lastName, int age, char gender, double salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
