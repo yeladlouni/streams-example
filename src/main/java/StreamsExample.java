a6e9d9076848b016663c4b55a60deb1b50f12c24
yassine <yeladlouni@gmail.com>
2023-09-07 15:10:49 +0100

    streams example

--- src/main/java/StreamsExample.java
+++ src/main/java/StreamsExample.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class StreamsExample {
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+public class StreamsExample {
+    public StreamsExample() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String, GenericRecord> stream = streamsBuilder.stream("customers");
+        stream.groupByKey()
+                .count(Materialized.as("count-store"))
+                .toStream()
+                .to("output-customers");
+
+    }
+
+    public static void main(String[] args) {
+        new StreamsExample();
+    }
 }
