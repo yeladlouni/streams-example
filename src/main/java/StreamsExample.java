889fbb9fb9f2d5ceab37c3e96893aceaed83bd50
yassine <yeladlouni@gmail.com>
2023-09-07 15:19:11 +0100

    added config

--- src/main/java/StreamsExample.java
+++ src/main/java/StreamsExample.java
@@ -1,5 +1,8 @@
 import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
@@ -8,17 +11,43 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Stream;
 
 public class StreamsExample {
     public StreamsExample() {
-        StreamsBuilder streamsBuilder = new StreamsBuilder();
-        KStream<String, GenericRecord> stream = streamsBuilder.stream("customers");
-        stream.groupByKey()
-                .count(Materialized.as("count-store"))
-                .toStream()
-                .to("output-customers");
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String, GenericRecord> stream = streamsBuilder.stream("customers");
+        stream.to("output-customers");
+
+        final Topology topology = streamsBuilder.build();
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 
     public static void main(String[] args) {
